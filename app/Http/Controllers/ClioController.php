07b8e320662692ac5d922a61554c20c4c0d21548
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-04 16:49:12 +0300

    init commit

---
+++ app/Http/Controllers/ClioController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class ClioController extends Controller
+{
+    public function index () {
+        return Socialite::driver('clio')->redirect();
+    }
+}
