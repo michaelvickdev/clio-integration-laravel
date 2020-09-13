6258a126be62f783e99813abe34062a422d3fd61
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-13 22:25:42 +0300

    update

--- app/Http/Controllers/Formstack.php
+++ app/Http/Controllers/Formstack.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class Formstack extends Controller
 {
     public function handleForm (Request $request) {
-        $input = $request->all();
+        $input = $request->getContent();
         dump(file_get_contents("php://input"));
         dd($input);
     }
