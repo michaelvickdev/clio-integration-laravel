a012583758bfc1c31e5bd56ac0e04521baef495d
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-13 22:45:37 +0300

    update

--- app/Http/Controllers/Formstack.php
+++ app/Http/Controllers/Formstack.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ClioApiTokens;
 
 class Formstack extends Controller
 {
     public function handleForm (Request $request) {
         $input = json_decode($request->getContent());
-        dump($input->HandshakeKey);
         if ($input->HandshakeKey == env('FORMSTACK_KEY')) {
-
+            $tokens = ClioApiTokens::find(1);
+            $url = env('CLIO_API_URL').'contacts.json';
+            $response = Http::withToken($tokens->access_token)->get($url);
+            dump($response);
         }
         dd($input);
     }
