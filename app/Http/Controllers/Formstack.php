69996d2fb8be842837ac11de1d3002b17306aaa6
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-13 22:23:37 +0300

    update

--- app/Http/Controllers/Formstack.php
+++ app/Http/Controllers/Formstack.php
@@ -8,6 +8,7 @@ class Formstack extends Controller
 {
     public function handleForm (Request $request) {
         $input = $request->all();
+        dump(file_get_contents("php://input"));
         dd($input);
     }
 }
