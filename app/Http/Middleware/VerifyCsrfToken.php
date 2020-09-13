1407e72e8d8a8caf4df2784224b6abb2a4393bf6
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-13 22:11:19 +0300

    update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'formstack/*',
+        'https://www.formstack.com/*'
     ];
 }
