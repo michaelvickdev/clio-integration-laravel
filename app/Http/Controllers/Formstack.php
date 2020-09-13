f631c5f80ccd9600150be6360cac2bdd146e4612
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-13 22:29:53 +0300

    update

--- app/Http/Controllers/Formstack.php
+++ app/Http/Controllers/Formstack.php
@@ -8,6 +8,10 @@ class Formstack extends Controller
 {
     public function handleForm (Request $request) {
         $input = json_decode($request->getContent());
+        dump($input->HandshakeKey);
+        if ($input->HandshakeKey == env('FORMSTACK_KEY')) {
+
+        }
         dd($input);
     }
 }
