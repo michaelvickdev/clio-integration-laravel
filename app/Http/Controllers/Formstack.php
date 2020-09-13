50f31c36354058e9da6121a22924ec83da10a7df
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-13 23:40:19 +0300

    update

--- app/Http/Controllers/Formstack.php
+++ app/Http/Controllers/Formstack.php
@@ -16,29 +16,28 @@ class Formstack extends Controller
             $url = env('CLIO_API_URL') . 'contacts.json';
             $getContacts = Http::withToken($tokens->access_token)->get($url);
             dump($getContacts->json());
-            //$data = \GuzzleHttp\json_encode(
-                $data =    [
-                    'data' => [
-                        "first_name" => $input->name->value->first,
-                        "middle_name" => $input->name->value->middle,
-                        "last_name" => $input->name->value->last,
-                        "email_addresses" => [
-                            [
-                                "name" => "Other",
-                                "address" => $input->email->value,
-                                "default_email" => true
-                            ]
-                        ],
-                        "phone_numbers" => [
-                            [
-                                "name" => "Other",
-                                "number" => $input->phone->value,
-                                "default_number" => true
-                            ]
-                        ],
-                    ]
-                ];
-            //);
+            $data = ['data' =>
+                [
+                    "first_name" => $input->name->value->first,
+                    "middle_name" => $input->name->value->middle,
+                    "last_name" => $input->name->value->last,
+                    "email_addresses" => [
+                        [
+                            "name" => "Other",
+                            "address" => $input->email->value,
+                            "default_email" => true
+                        ]
+                    ],
+                    "phone_numbers" => [
+                        [
+                            "name" => "Other",
+                            "number" => $input->phone->value,
+                            "default_number" => true
+                        ]
+                    ],
+                    "type" => "Person",
+                ]
+            ];
             dump($data);
             $createContact = Http::withToken($tokens->access_token)
                 ->withHeaders(['Content-Type' => 'application/json'])
