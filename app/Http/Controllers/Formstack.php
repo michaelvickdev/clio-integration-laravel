ae3bb45554d34c0106249a5612f42905d04414aa
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-13 23:03:27 +0300

    update

--- app/Http/Controllers/Formstack.php
+++ app/Http/Controllers/Formstack.php
@@ -8,13 +8,40 @@ use Illuminate\Support\Facades\Http;
 
 class Formstack extends Controller
 {
-    public function handleForm (Request $request) {
+    public function handleForm(Request $request)
+    {
         $input = json_decode($request->getContent());
         if ($input->HandshakeKey == env('FORMSTACK_KEY')) {
             $tokens = ClioApiTokens::find(1);
-            $url = env('CLIO_API_URL').'contacts.json';
-            $response = Http::withToken($tokens->access_token)->get($url);
-            dump($response);
+            $url = env('CLIO_API_URL') . 'contacts.json';
+            $getContacts = Http::withToken($tokens->access_token)->get($url);
+            dump($getContacts->json());
+            $data = \GuzzleHttp\json_encode(
+                [
+                    'data' => [
+                        "first_name" => $input->name->value->first,
+                        "middle_name" => $input->name->value->middle,
+                        "last_name" => $input->name->value->last,
+                        "email_addresses" => [
+                            [
+                                "name" => "Other",
+                                "address" => $input->email->value,
+                                "default_email" => true
+                            ]
+                        ],
+                        "phone_numbers" => [
+                            [
+                                "name" => "Other",
+                                "number" => $input->phone->value,
+                                "default_number" => true
+                            ]
+                        ],
+                    ]
+                ]
+            );
+            dump($data);
+            $createContact = Http::withToken($tokens->access_token)->withBody($data)->post($url);
+            dump($createContact->json());
         }
         dd($input);
     }
