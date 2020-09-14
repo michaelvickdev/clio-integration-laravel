fc154fee594df14554063f9e6d257518b4c47f92
michaelvickdev <michael.vick.hello@gmail.com>
2020-09-14 11:27:47 +0300

    update

--- app/Http/Controllers/Formstack.php
+++ app/Http/Controllers/Formstack.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use http\Client\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 use App\ClioApiTokens;
 use Illuminate\Support\Facades\Http;
